4c35b298f20c5b6466913d835d4ee01750ff8877
larry2004h-hash <larry2004h@gmail.com>
2025-11-09 19:46:15 -0500

    compilacion de main

--- src/LaboratorioFungi/AppLaboratorioFungi.java
+++ src/LaboratorioFungi/AppLaboratorioFungi.java
@@ -12,6 +12,7 @@ public class AppLaboratorioFungi {
 
     // Constructor
     public AppLaboratorioFungi() {
+        System.out.println("Construyendo el Laboratorio de Fungi...");
         micologo1 = new Micologo("1234567890", "Juan", "Perez");
         recolector1 = new Recolector("Carlos", "Ramirez");
         recolector2 = new Recolector("0987654321", "Ana", "Gomez");
@@ -19,6 +20,8 @@ public class AppLaboratorioFungi {
 
     // Método principal del sistema
     public void iniciarLaboratorio() {
+        System.out.println("Iniciando el Laboratorio de Fungi...");
+        micologo1.setLogin("micologo_jhoan");
         micologo1.setClave("micologia123");
         recolector2.recolectarMuestra("Cantharellus cibarius");
         micologo1.cultivarHongos();
