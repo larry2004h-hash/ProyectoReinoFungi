7d84b49eb70d749a111b278b382aa86b6723949d
IngeRoman <deyvid.roman@epn.edu.ec>
2025-11-09 19:39:21 -0500

    Actualizar Moho.java y Levadura.java con contenido completo

--- src/LaboratorioFungi/pkSerViviente/pkReinoFungi/Levadura.java
+++ src/LaboratorioFungi/pkSerViviente/pkReinoFungi/Levadura.java
@@ -1,5 +1,34 @@
 package LaboratorioFungi.pkSerViviente.pkReinoFungi;
 
-public abstract class Levadura {
+public abstract class Levadura extends Fungi {
 
+    private Float tamanoCelular;
+    private String metabolismo;
+
+    public Levadura(String nombreCientifico, Float tamanoCelular, String metabolismo) {
+        super(nombreCientifico);
+        this.tamanoCelular = tamanoCelular;
+        this.metabolismo = metabolismo;
+    }
+
+    public Float getTamanoCelular() {
+        return tamanoCelular;
+    }
+
+    public void setTamanoCelular(Float tamanoCelular) {
+        this.tamanoCelular = tamanoCelular;
+    }
+
+    public String getMetabolismo() {
+        return metabolismo;
+    }
+
+    public void setMetabolismo(String metabolismo) {
+        this.metabolismo = metabolismo;
+    }
+
+    public void fermentar() {
+        System.out.println("La levadura " + getNombreCientifico() + " está fermentando");
+    }
 }
+
--- src/LaboratorioFungi/pkSerViviente/pkReinoFungi/Moho.java
+++ src/LaboratorioFungi/pkSerViviente/pkReinoFungi/Moho.java
@@ -1,5 +1,34 @@
 package LaboratorioFungi.pkSerViviente.pkReinoFungi;
 
-public abstract class Moho {
+public abstract class Moho extends Fungi {
 
+    private String tipoHifa;
+    private Float velocidadCrecimiento;
+
+    public Moho(String nombreCientifico, String tipoHifa, Float velocidadCrecimiento) {
+        super(nombreCientifico);
+        this.tipoHifa = tipoHifa;
+        this.velocidadCrecimiento = velocidadCrecimiento;
+    }
+
+    public String getTipoHifa() {
+        return tipoHifa;
+    }
+
+    public void setTipoHifa(String tipoHifa) {
+        this.tipoHifa = tipoHifa;
+    }
+
+    public Float getVelocidadCrecimiento() {
+        return velocidadCrecimiento;
+    }
+
+    public void setVelocidadCrecimiento(Float velocidadCrecimiento) {
+        this.velocidadCrecimiento = velocidadCrecimiento;
+    }
+
+    public void formarMicelio() {
+        System.out.println("El moho " + getNombreCientifico() + " está formando micelio.");
+    }
 }
+
